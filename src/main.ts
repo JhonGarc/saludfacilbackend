902074a10ae0ef5a705b43c7ed920d9f20b85769
Jhon Garcia <89317228+JhonGarc@users.noreply.github.com>
2025-07-28 11:40:53 -0500

    cors verdaderos

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors(process.env.PORT ?? 3000);
+  app.enableCors();
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
